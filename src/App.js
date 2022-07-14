597cad0215538886899a3ec2cf7ef946370fef74
SkarletA <skarlet15m@gmail.com>
2022-07-13 23:00:34 -0500

    config api and using Redux for pokemon Api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Col } from 'antd';
+import Search from './components/Search';
+import PokemonList from './components/PokemonList';
+import logo from './statics/logo.svg'
 import './App.css';
+import { getPokemon } from './api';
+import { setPokemons as setPokemonActions} from './actions';
 
-function App() {
+function App({ pokemons, setPokemons }) {
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonRes = await getPokemon();
+      setPokemons(pokemonRes);
+    }
+    fetchPokemons();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Col span={6} offset={10}>
+        <img src={logo} alt="Pokedux"/>
+      </Col>
+      <Col span={8} offset={8}>
+        <Search />
+      </Col>
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
-
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonActions(value)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
