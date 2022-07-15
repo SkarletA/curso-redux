1547074a8a1c96a0dc7ea721de2b842ed55b5142
SkarletA <skarlet15m@gmail.com>
2022-07-14 22:59:41 -0500

    working with redux tools and add image pokemon

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import {  logger } from './middleware';
+import rootReducer from './reducers/rootReducer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(pokemonsReducer);
+const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = composeAlt(
+  applyMiddleware(thunk, logger)
+);
+const store = createStore(rootReducer, composeEnhancers);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
